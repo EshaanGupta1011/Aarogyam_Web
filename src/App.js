2134da854188f76e4a69af8551836f6889dadeaa
Eshaan Gupta <eshaan.gupta.33@gmail.com>
2023-10-16 17:14:43 +0530

    made the features section. Have to change a lot

--- src/App.js
+++ src/App.js
@@ -15,6 +15,65 @@ export default function App() {
       <div className="section-aboutus">
         <AboutUs />
       </div>
+      <div className="section-features">
+        <Features />
+      </div>
+    </div>
+  );
+}
+
+function Features() {
+  const content = [
+    {
+      imgUrl: "./run.png",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,.",
+      colour: "#ffe3e3",
+    },
+    {
+      imgUrl: "./med.png",
+      colour: "#d3f9d8",
+
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,.",
+    },
+    {
+      imgUrl: "./yoga.png",
+      colour: "#ffe8cc",
+
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,.",
+    },
+    {
+      imgUrl: "./connect.png",
+      colour: "#c5f6fa",
+
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,.",
+    },
+  ];
+
+  return (
+    <div>
+      <h1 className="secondary-heading features-secondary-heading">
+        How does Aarogyam helps you?
+      </h1>
+
+      <div className="grid features-grid">
+        {content.map((s) => (
+          <FeaturesList imgUrl={s.imgUrl} text={s.text} colour={s.colour} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function FeaturesList({ imgUrl, text, colour }) {
+  return (
+    <div className="flex features-flex">
+      <img
+        src={imgUrl}
+        alt={text}
+        className="features-img"
+        style={{ backgroundColor: colour }}
+      />
+      <p className="features-text">{text}</p>
     </div>
   );
 }
