63767c53a9f935508d877b8096ab8d239301ed6c
Eshaan Gupta <eshaan.gupta.33@gmail.com>
2023-10-16 15:51:19 +0530

    Changed the write up of about us section

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -4,13 +4,12 @@ export function AboutUs() {
       <div>
         <h1 className="secondary-heading about-secondary-heading">About us</h1>
         <p className="about-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Aarogyam, meaning 'Health,' is more than just a fitness app. Here,
+          your well-being, with a special emphasis on mental health, takes
+          center stage. Our aim is to provide effective and convenient health
+          support. We strongly believe that nurturing the mind is the
+          cornerstone to a fulfilling life, and we are wholeheartedly dedicated
+          to assisting you on this journey
         </p>
       </div>
 
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -4,7 +4,7 @@ export function Hero() {
       <div className="hero-heading">
         <h1 className="primary-heading hero-primary-heading">Aarogyam</h1>
         <h2 className="secondary-heading hero-secondary-heading">
-          Your own holistic health app!
+          Your Holistic Health Companion
         </h2>
       </div>
     </>
